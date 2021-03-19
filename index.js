e84a07e98d0672d56383113e9cd180ec3a6053aa
Deboy <55631825@qq.com>
2021-03-19 10:39:52 +0800

    feat: updateu esbuild-loader version

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const { ESBuildPlugin } = require('esbuild-loader')
-
 const { assign } = Object
 const kRegistered = Symbol.for('registered')
 
@@ -16,7 +14,6 @@ module.exports = function (moduleOptions = {}) {
     }
     const originalUse = config.module.rules[jsxRuleIndex].use
     if (this.options.dev || options.loader === 'ts') {
-      config.plugins.push(new ESBuildPlugin())
       config.module.rules.splice(jsxRuleIndex, 1, {
         test: /\.((m?jsx?)|(ts))$/,
         use: [
